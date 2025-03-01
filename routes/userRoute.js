07bf19c514dea1e556d51238ba234343dd1ae4e1
Ruchi Gupta <165932914+ruchigupta-12345@users.noreply.github.com>
2025-03-02 00:02:19 +0530

    Delete routes directory

--- routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {loginUser , registeUser} from "../controllers/userController.js"
-
-const userRouter = express.Router()
-
-userRouter.post("/register",registeUser)
-userRouter.post("/login",loginUser)
-
-export default userRouter;
